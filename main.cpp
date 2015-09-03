19a50e97004dc25e2ad552b823338784acbbc155
Adam Kilbo <amkilbo@gmail.com>
2015-09-03 12:50:00 -0700

    cleaned up whitespace and comments

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 	cout << "Hello, welcome to the adder/subtractor." << endl;
-	cout << "If you would like to add two numbers, enter one. Otherwise, enter 2" << endl;
+	cout << "If you would like to add two numbers, enter '1'. Otherwise, enter '2'" << endl;
 	int input = 0; 
 	while (input != 1 || 2) //loop to ensure a valid input
 	{
@@ -16,14 +16,14 @@ int main()
 		{
 			add();
 			break;
-			//code for moving to adder file goes here
 		}
+		
 		else if (input == 2) //if true, user specified to subtract
 		{
 			subtract();
 			break;
-			//code for moving to subtractor file goes here.
 		}
+
 		else
 		{
 			cout << "input not recognized, please enter '1' for adding or '2' for subtracting" << endl;
